1862aa292fdc5013d4c1e7f15d83a1d597cb6c1e
Antti Kivi <antti.kivi@visiosto.fi>
2020-10-30 17:24:10 +0200

    Create function for creating the multilingual pages

---
+++ gatsby-node.js
@@ -0,0 +1,88 @@
+// Copyright (c) 2020 Antti Kivi
+// Licensed under the MIT License
+
+const fs = require("fs");
+const path = require("path");
+
+const recursiveReadDirSync = folderPath => {
+  if (!fs.existsSync(folderPath)) {
+    return [];
+  }
+
+  const entryPaths = fs
+    .readdirSync(folderPath)
+    .map(entry => path.join(folderPath, entry));
+  const filePaths = entryPaths.filter(entryPath =>
+    fs.statSync(entryPath).isFile()
+  );
+  const dirPaths = entryPaths.filter(
+    entryPath => !filePaths.includes(entryPath)
+  );
+  const dirFiles = dirPaths.reduce(
+    (prev, curr) => prev.concat(recursiveReadDirSync(curr)),
+    []
+  );
+
+  return [...filePaths, ...dirFiles]
+    .filter(f => !f.endsWith(".DS_Store") && !f.endsWith("README.md"))
+    .map(f => {
+      const root = path.join(__dirname, "..", "..", "..");
+      return f.replace(root, "");
+    });
+};
+
+const createRootPages = async createPage => {
+  const rootPagesDir = path.join(__dirname, "src", "templates");
+  const langRootDir = path.join(__dirname, "src", "locales");
+
+  const langs = fs
+    .readdirSync(langRootDir)
+    .filter(f => f.endsWith(".js") && f.length === 5)
+    .map(f => path.basename(f, ".js"));
+
+  const files = recursiveReadDirSync(rootPagesDir)
+    .filter(f => !f.startsWith("."))
+    .filter(f => !f.includes(".scss"));
+
+  files.forEach(f => {
+    const fullpath = path.join(__dirname, "..", "..", "..", f);
+    let originalSitePath = path
+      .relative(rootPagesDir, fullpath)
+      .replace(/.js$/g, "");
+
+    // Remove the index files
+    if (originalSitePath.endsWith("index")) {
+      originalSitePath = originalSitePath.substring(
+        0,
+        originalSitePath.length - 5
+      );
+    }
+    // If they have .fi then just drop that completely
+    if (originalSitePath.endsWith(".fi")) {
+      originalSitePath = originalSitePath.substring(
+        0,
+        originalSitePath.length - 3
+      );
+    }
+
+    langs.forEach(lang => {
+      const prefix = lang === "fi" ? "/" : `/${lang}/`;
+      const sitePath = `${prefix}${originalSitePath}`;
+      const pageOpts = {
+        path: sitePath,
+        component: fullpath,
+        context: {
+          lang: lang === "" ? "fi" : lang
+        }
+      };
+
+      createPage(pageOpts);
+    });
+  });
+};
+
+exports.createPages = async ({actions}) => {
+  const {createPage} = actions;
+
+  await createRootPages(createPage);
+};
