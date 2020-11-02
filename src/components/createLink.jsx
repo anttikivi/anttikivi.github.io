84dbf26b1e3bc2a4e7b079fbb94838d4410460c0
Antti Kivi <antti.kivi@visiosto.fi>
2020-11-02 13:12:04 +0200

    Remove the excessive logging from the links

--- src/components/createLink.jsx
+++ src/components/createLink.jsx
@@ -16,14 +16,6 @@ const createLink = currentLocale => {
     const pageKey = linkProps.to.substring(1);
     let to = linkProps.to;
 
-    console.log(
-      "Going to construct link in locale",
-      currentLocale,
-      "to",
-      linkProps.to
-    );
-    console.log("The page key is", pageKey);
-
     if (pageKey in slugs && currentLocale in slugs[pageKey]) {
       to = `/${slugs[pageKey][currentLocale]}`;
     }
@@ -31,14 +23,9 @@ const createLink = currentLocale => {
     const localeVersion =
       pageKey === "" ? `/${currentLocale}` : `/${currentLocale}${to}`;
 
-    console.log("The locale version of the path is", localeVersion);
-    console.log("The paths include", paths);
-
     if (currentLocale !== "fi" && paths.includes(localeVersion)) {
       to = localeVersion;
     }
-
-    console.log("The link points to", to);
 
     return <Link {...linkProps} to={to} />;
   };
