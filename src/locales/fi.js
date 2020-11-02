9c4a10445e99b6e5d17a7476192bdbbcaa4f21b3
Antti Kivi <antti.kivi@visiosto.fi>
2020-11-02 11:05:54 +0200

    Internationalize the curriculum vitae page and add Finnish translation

--- src/locales/fi.js
+++ src/locales/fi.js
@@ -3,12 +3,14 @@
 
 import {defineMessages} from "react-intl";
 
+import {curriculumVitaeMessages} from "./fi/curriculum-vitae";
 import {footerMessages} from "./fi/footer";
 import {headerMessages} from "./fi/header";
 import {indexMessages} from "./fi/index";
 import {navigationMessages} from "./fi/navigation";
 
 export const messages = {
+  ...curriculumVitaeMessages,
   ...footerMessages,
   ...headerMessages,
   ...indexMessages,
