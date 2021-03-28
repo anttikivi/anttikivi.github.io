0130a1d00ff9597cd01277cb4411ceadaad882d3
Antti Kivi <antti.kivi@visiosto.fi>
2021-03-29 02:50:33 +0300

    feat: update the translations

--- src/locales/en.js
+++ src/locales/en.js
@@ -1,24 +1,16 @@
 // Copyright (c) 2020 Antti Kivi
 // Licensed under the MIT License
 
-import {defineMessages} from "react-intl";
+import { defineMessages } from 'react-intl';
 
-import {curriculumVitaeMessages} from "./en/curriculum-vitae";
-import {footerMessages} from "./en/footer";
-import {headerMessages} from "./en/header";
-import {indexMessages} from "./en/index";
-import {metaMessages} from "./en/meta";
-import {navigationMessages} from "./en/navigation";
-import {toggleButtonMessages} from "./en/toggle-button";
+import footerMessages from './en/footer';
+import headerMessages from './en/header';
+import metaMessages from './en/meta';
 
 export const messages = {
-  ...curriculumVitaeMessages,
   ...footerMessages,
   ...headerMessages,
-  ...indexMessages,
   ...metaMessages,
-  ...navigationMessages,
-  ...toggleButtonMessages
 };
 
-export const lang = defineMessages(messages);
+export const locale = defineMessages(messages);
--- src/locales/en/toggle-button.js
+++
@@ -1,9 +0,0 @@
-// Copyright (c) 2020 Antti Kivi
-// Licensed under the MIT License
-
-export const toggleButtonMessages = {
-  /* eslint-disable camelcase */
-  toggle_button_hide: "Hide",
-  toggle_button_show: "Show"
-  /* eslint-enable camelcase */
-};
