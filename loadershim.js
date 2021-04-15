49c7e1ae2d42be13d5510d6f75dcdd6a175cb672
Antti Kivi <antti.kivi@visiosto.fi>
2021-04-15 18:44:49 +0300

    test: add initial framework for unit tests

---
+++ loadershim.js
@@ -0,0 +1,6 @@
+// Copyright (c) 2021 Antti Kivi
+// Licensed under the MIT License
+
+global.___loader = {
+  enqueue: jest.fn(),
+}
---
+++ setup-test-env.js
@@ -0,0 +1,4 @@
+// Copyright (c) 2021 Antti Kivi
+// Licensed under the MIT License
+
+import '@testing-library/jest-dom/extend-expect';
