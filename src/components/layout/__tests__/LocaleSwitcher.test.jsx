a030b82aedabf5490b69af3664d82a38934b0853
Antti Kivi <antti.kivi@visiosto.fi>
2021-04-16 11:18:16 +0300

    test: add locale switcher test

---
+++ src/components/layout/__tests__/LocaleSwitcher.test.jsx
@@ -0,0 +1,41 @@
+// Copyright (c) 2021 Antti Kivi
+// Licensed under the MIT License
+
+import React from 'react';
+import { screen } from '@testing-library/react';
+import { useStaticQuery } from 'gatsby';
+
+import '../../../../__mocks__/matchMedia';
+
+import LocaleSwitcher from '../LocaleSwitcher';
+
+import localizedLinkQuery from '../../../../test/data/localizedLinkQuery';
+import renderWithProviders from '../../../../test/renderWithProviders';
+
+describe('Locale switcher component', () => {
+  beforeAll(() => {
+    useStaticQuery.mockReturnValue(localizedLinkQuery);
+  });
+
+  it('renders Finnish index switcher correctly', () => {
+    const { container, getByText } = renderWithProviders(
+      <LocaleSwitcher locale="fi" pageID="6JksITICuGCEYUIVHlWl5U" />,
+      'fi',
+    );
+
+    expect(container).toMatchSnapshot();
+
+    expect(getByText('In English')).toBeInTheDocument();
+  });
+
+  it('renders English index switcher correctly', () => {
+    const { container, getByText } = renderWithProviders(
+      <LocaleSwitcher locale="en-GB" pageID="6JksITICuGCEYUIVHlWl5U" />,
+      'en',
+    );
+
+    expect(container).toMatchSnapshot();
+
+    expect(getByText('Suomeksi')).toBeInTheDocument();
+  });
+});
