2abd0d69705e7cad2aee5e776f33990914d21227
Antti Kivi <antti.kivi@visiosto.fi>
2021-04-20 12:37:05 +0300

    fix: fix the not found page by using external component

--- src/templates/NotFound.jsx
+++ src/templates/NotFound.jsx
@@ -4,27 +4,10 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { graphql } from 'gatsby';
-import styled from 'styled-components';
-import { useIntl } from 'react-intl';
 
 import Intl from '../components/Intl';
-import LayoutError from '../components/layout/LayoutError';
+import NotFoundPage from '../components/NotFoundPage';
 import Theme from '../components/Theme';
-
-import createInternationalization from '../util/createInternationalization';
-
-const Div = styled.div`
-  margin: 2em ${(props) => props.theme.layout.marginMobile};
-  text-align: center;
-
-  @media screen and (${(props) => props.theme.devices.mobileL}) {
-    margin: 2em ${(props) => props.theme.layout.marginTablet};
-  }
-
-  @media screen and (${(props) => props.theme.devices.tablet}) {
-    margin: 2em ${(props) => props.theme.layout.marginDesktop};
-  }
-`;
 
 const propTypes = {
   // eslint-disable-next-line react/forbid-prop-types
@@ -36,18 +19,13 @@ const propTypes = {
 };
 
 function NotFound({ data, pageContext }) {
-  const intl = createInternationalization(useIntl());
   const { simpleLocales } = data.site.siteMetadata;
-  const { locale, pageID } = pageContext;
+  const { locale } = pageContext;
 
   return (
     <Intl locale={simpleLocales[locale.replace('-', '_')]}>
       <Theme>
-        <LayoutError errorCode="404" locale={locale} pageID={pageID} title={intl('notFoundTitle')}>
-          <Div>
-            <p>{intl('notFoundContent')}</p>
-          </Div>
-        </LayoutError>
+        <NotFoundPage pageContext={pageContext} />
       </Theme>
     </Intl>
   );
