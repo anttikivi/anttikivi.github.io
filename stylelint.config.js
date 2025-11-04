11074fa8ea4c251eebce7cc1ad194a863f0a712a
Antti Kivi <antti@anttikivi.com>
2025-11-04 23:37:16 +0200

    style: fix lint errors

--- stylelint.config.js
+++ stylelint.config.js
@@ -2,7 +2,6 @@
 export default {
     extends: ["stylelint-config-standard"],
     rules: {
-        "import-notation": null,
         "at-rule-no-unknown": [
             true,
             {
@@ -11,5 +10,14 @@ export default {
         ],
         "custom-property-pattern": null,
         "custom-property-empty-line-before": null,
+        "declaration-property-value-no-unknown": [
+            true,
+            {
+                ignoreProperties: {
+                    "/.+/": ["/^--spacing\\(.+\\)/"],
+                },
+            },
+        ],
+        "import-notation": null,
     },
 };
