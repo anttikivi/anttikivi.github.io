a6968d3037947be428cefff5f4d2f15fd7557bc0
Antti Kivi <antti@anttikivi.com>
2025-11-05 17:19:50 +0200

    feat: add security.txt

---
+++ src/pages/.well-known/security.txt.ts
@@ -0,0 +1,12 @@
+import locales, { getLang } from "@/locales";
+import { APIRoute } from "astro";
+
+export const GET: APIRoute = (context) =>
+    new Response(
+        `Contact: mailto:antti@anttikivi.com
+Expires: 2025-12-31T09:45:00.000Z
+Encryption: ${context.site}pgp.asc
+Preferred-Languages: ${locales.map((locale) => getLang(locale)).join(", ")}
+Canonical: ${context.url}
+`.trim(),
+    );
